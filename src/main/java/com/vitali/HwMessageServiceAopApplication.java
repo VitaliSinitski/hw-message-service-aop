ebe957b0d27dd74da0b9cf60225b0525cd6f71a3
Vitali Sinitski <vitali.sin777@gmail.com>
2023-04-19 23:51:24 +0300

    Initial commit

---
+++ src/main/java/com/vitali/HwMessageServiceAopApplication.java
@@ -0,0 +1,13 @@
+package com.vitali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwMessageServiceAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwMessageServiceAopApplication.class, args);
+	}
+
+}
